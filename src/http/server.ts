ec2f2c57ae5e2f51a73643379d19460e5341ad03
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-12-24 22:58:09 -0300

    Cria a rota de criação de uma nova meta

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,14 +1,40 @@
 import fastify from "fastify"
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod"
 import { env } from "../env"
+import { createGoal } from "../use-cases/create-goal"
+import z from "zod"
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-const PORT = env.PORT
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+const validatorCompilerBody = {
+  schema: {
+    body: z.object({
+      title: z.string(),
+      desiredWeeklyFrequency: z.number().int().min(1).max(7),
+    }),
+  },
+}
+
+app.post("/goals", validatorCompilerBody, async (req, res) => {
+  const { title, desiredWeeklyFrequency } = req.body
+
+  await createGoal({
+    title,
+    desiredWeeklyFrequency,
+  })
+})
 
 app
   .listen({
-    port: PORT,
+    port: env.PORT,
   })
   .then(() => {
-    console.log(`listening on port ${PORT}`)
+    console.log(`listening on port ${env.PORT}`)
   })
