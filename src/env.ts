3c35c403b60f2876b5e3fe10b7888f749252196b
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-12-24 21:11:43 -0300

    Configura a variável de ambiente para a porta de execução usada pelo servidor

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,7 @@ import z from "zod"
 
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
+  PORT: z.coerce.number().default(3333).transform(port => port === 0 ? 3333 : port),
 })
 
 export const env = envSchema.parse(process.env)
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,9 @@
 import fastify from "fastify"
+import { env } from "../env"
 
 const app = fastify()
 
-const PORT = 3000
+const PORT = env.PORT
 
 app
   .listen({
